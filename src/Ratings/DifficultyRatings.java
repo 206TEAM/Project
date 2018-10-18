e6fa9d1ae67e02fea1ddf19112aae2da4f474414
Lucy <lucy.chen.nz@gmail.com>
2018-10-18 21:36:51 +1300

    cleaned up difficulty

--- src/Ratings/DifficultyRatings.java
+++ src/Ratings/DifficultyRatings.java
@@ -19,7 +19,6 @@ public class DifficultyRatings {
     private List<String> _hardNames;
     private List<String> _ascending;
 
-
     /**
      * creates a model for the difficulty ratings of the names
      */
@@ -28,7 +27,6 @@ public class DifficultyRatings {
         _easyNames = new ArrayList<String>();
         _mediumNames = new ArrayList<String>();
         _hardNames = new ArrayList<String>();
-
 
         List<String> names = Originals.getInstance().listNames();
         _ascending = sortNameOrder(names, true);
@@ -44,36 +42,36 @@ public class DifficultyRatings {
         return instance;
     }
 
+    /**
+     * this method sorts the names into their respective lists (easy, medium and hard)
+     * It divides up the name list into 3 sections based on the length of the names.
+     * (longer names are harder, and shorter names are easier).
+     */
     private void sortNames() {
-        int listSize = Originals.getInstance().listNames().size();
+        int listSize = _ascending.size();
         if (listSize == 0) {
 
         } else if (listSize == 1 || listSize == 2) {
             for (String name : _ascending) {
                 _mediumNames.add(name);
-                //_defaultNames.put(name, Difficulty.MEDIUM);
             }
         } else {
             int slice = (int) Math.floor(listSize / 3);
             for (int i = 0; i < slice; i++) {
                 _easyNames.add((_ascending.get(i)));
-                //_defaultNames.put(_ascending.get(i), Difficulty.EASY);
             }
             for (int i = slice; i < slice * 2; i++) {
                 _mediumNames.add((_ascending.get(i)));
-                // _defaultNames.put(_ascending.get(i), Difficulty.MEDIUM);
             }
             for (int i = slice * 2; i < listSize; i++) {
                 _hardNames.add((_ascending.get(i)));
-                //_defaultNames.put(_ascending.get(i), Difficulty.HARD);
             }
         }
     }
 
     /**
-     * This method sorts the names by their name lengths
-     *
-     * @return
+     * This method sorts a list of strings by their string length. can be in ascending or descending order.
+     * returns sorted list.
      */
     private List<String> sortNameOrder(List<String> names, Boolean ascending) {
         Comparator c = new Comparator<String>() {
@@ -93,7 +91,7 @@ public class DifficultyRatings {
 
     /**
      * This method updates the difficulty rating of a name.
-     * it also updates the lists
+     * it also updates the lists (easy, medium, hard).
      */
     public void setRating(String nameKey, Boolean difficulty) {
         _nameRatings.put(nameKey, difficulty);
@@ -104,7 +102,7 @@ public class DifficultyRatings {
             }
             _easyNames.add(nameKey); //todo should it be easy?
 
-        } else { //user set rating to be hard
+        } else { //user set rating of name to be hard
             if (_easyNames.contains(nameKey)) {
                 _easyNames.remove(nameKey);
 
@@ -131,12 +129,11 @@ public class DifficultyRatings {
     private void updateModel() { //todo
     }
 
-
     /**
-     * This method generates a list of the names
+     * This method generates a challenge list based on the difficulty of the session and the
+     * number of names required for that session.
      */
     public List<String> generateList(int difficultySession, int number) {
-        System.out.println("before hard names are \n" + _hardNames);
         if (number > _ascending.size()) { //if number is too big
             return _ascending;
         }
@@ -145,7 +142,6 @@ public class DifficultyRatings {
             case 1:
                 return getDiffList(_easyNames, _mediumNames, _hardNames, number, true, false);
             case 2:
-                //
                 return getDiffList(_easyNames, _mediumNames, _hardNames, number, false, true);
             case 3:
                 return getDiffList(_mediumNames, _hardNames, _easyNames, number, false, false);
@@ -157,6 +153,22 @@ public class DifficultyRatings {
         return null;
     }
 
+    /**
+     * this method returns the list of names based on several conditions.
+     * if difficulty is 1 or 5 (extreme cases), then it takes as much of the easy/hard lists for the challenge
+     * it then takes as much medium names to fill up, and then finally easy names, on the condition that the number
+     * of names do not exceed the required number.
+     * if difficulty is 2 or 4 (middle cases), it uses 40% for easy/hard, a further 40% for medium and the remainder 20%
+     * for hard/easy. if names run out, it then takes a random portion of the remaining names left to get to the required
+     * number of names.
+     * if difficulty is 3, it uses 50% for medium names, and 25% for easy and hard to ensure for a balance of names.
+     * @param first: dominant list of names (if difficulty is 1,2), then it would be easy
+     * @param second middle for difficulty levels 1,2,4,5
+     * @param third recessive list of names
+     * @param number number of challenge names required
+     * @param extreme true for difficulty 1,5
+     * @param middle true for difficulty 2,4
+     */
     private List<String> getDiffList(List<String> first, List<String> second, List<String> third, int number, Boolean extreme, Boolean middle) {
         List<String> challengeList = new ArrayList<String>();
         List<String> remaining = _ascending;
@@ -169,13 +181,13 @@ public class DifficultyRatings {
         int secondIndex;
 
         if (middle) {
-            firstIndex = (int) Math.floor(number * 0.4);
+            firstIndex = (int) Math.ceil(number * 0.4);
             secondIndex = (int) Math.floor(number * 0.2);
         } else if (extreme) {
             firstIndex = number;
             secondIndex = second.size();
         } else {
-            firstIndex = (int) Math.floor(number * 0.5);
+            firstIndex = (int) Math.ceil(number * 0.5);
             secondIndex = (int) Math.floor(number * 0.25);
         }
 
@@ -200,23 +212,18 @@ public class DifficultyRatings {
             i++;
         }
 
+        /**
+         * if still more names need to be filled up, this method adds the remaining names.
+         */
         i = 0;
         while (challengeList.size() < number) {
             challengeList.add(remaining.get(i)); //gets the dominant
             remaining.remove(first.get(i));
             i++;
         }
-
-        printstuff();
         return challengeList;
     }
 
-    public void printstuff(){
-        System.out.println("easynames are \n" + _easyNames);
-        System.out.println("mednames are \n" + _mediumNames);
-        System.out.println("hardnames are \n" + _hardNames);
-
-    }
 }
 
 
