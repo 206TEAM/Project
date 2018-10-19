05019295aefc01ef73b39df34cf95493763f69a1
Eric Pedrido <epedrids@gmail.com>
2018-10-19 13:18:07 +1300

    Concat names from the search bar.

--- src/Control/SelectPracticeController.java
+++ src/Control/SelectPracticeController.java
@@ -1,5 +1,7 @@
 package Control;
 
+import Model.Media;
+import Model.Original;
 import Model.Originals;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -10,12 +12,17 @@ import javafx.scene.control.*;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.text.Font;
 import javafx.stage.FileChooser;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -30,17 +37,20 @@ public class SelectPracticeController extends Controller {
 	@FXML public TextField search;
 	@FXML public MenuButton sortBy;
 	@FXML public MenuItem selected, alphabetical;
+	@FXML public Label nameText;
 
 	private static SelectPracticeController _INSTANCE;
-	private List<String> selectedOrder;
-	private List<String> currentPreviewList;
+	private List<String> _selectedOrder;
+	private List<String> _currentPreviewList;
+	private List<String> _names;
+	private String _newName;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		_INSTANCE = this;
 		disableButtons(true);
-		selectedOrder = new ArrayList<>();
-		currentPreviewList = new ArrayList<>();
+		_selectedOrder = new ArrayList<>();
+		_currentPreviewList = new ArrayList<>();
 
 		List<String> names = _originals.listNames();
 
@@ -66,12 +76,19 @@ public class SelectPracticeController extends Controller {
 			selectListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
 
 			search.textProperty().addListener(((observable, oldValue, newValue) -> {
-				searchListener(filteredList, newValue, selectListView);
-				if (!newValue.equals("")) {
+				selectListView.setItems(filteredList);
+				if (!newValue.equals("") && !newValue.contains(" ")) {
 					add.setDisable(false);
+					searchListener(filteredList, newValue, selectListView);
 					selectListView.getSelectionModel().selectFirst();
+					setNameText("Enter another name to combine", Color.BLACK, true);
+				} else if (newValue.contains(" ")) {
+					selectListView.setItems(practiceNames);
+					setLabelText(newValue);
 				} else {
 					add.setDisable(true);
+					nameText.setText("");
+					selectListView.setItems(practiceNames);
 				}
 			}));
 		}
@@ -103,7 +120,7 @@ public class SelectPracticeController extends Controller {
 	public void reset(ActionEvent actionEvent) {
 		_mediator.setPracticeMainList(new ArrayList<>());
 		previewList.getItems().clear();
-		selectedOrder.clear();
+		_selectedOrder.clear();
 
 		disableButtons(true);
 		searchOpacity(true);
@@ -116,10 +133,10 @@ public class SelectPracticeController extends Controller {
 			FXCollections.shuffle(previewList.getItems());
 		} else {
 			sortBy.setDisable(false);
-			if (currentPreviewList.size() == 0) {
-				currentPreviewList = selectedOrder;
+			if (_currentPreviewList.size() == 0) {
+				_currentPreviewList = _selectedOrder;
 			}
-			previewList.getItems().setAll(currentPreviewList);
+			previewList.getItems().setAll(_currentPreviewList);
 		}
 		searchOpacity(true);
 	}
@@ -131,7 +148,7 @@ public class SelectPracticeController extends Controller {
 		if (selectedItem != null) {
 			if (!previewList.getItems().contains(selectedItem)) {
 				previewList.getItems().add(selectedItem);
-				selectedOrder.add(selectedItem);
+				_selectedOrder.add(selectedItem);
 			}
 			disableButtons(false);
 		}
@@ -144,7 +161,7 @@ public class SelectPracticeController extends Controller {
 		if (selectedItem != null) {
 			if (mouseEvent.getClickCount() == 2) {
 				previewList.getItems().remove(selectedItem);
-				selectedOrder.remove(selectedItem);
+				_selectedOrder.remove(selectedItem);
 				_mediator.setPracticeMainList(previewList.getItems());
 				if (previewList.getItems().size() == 0) {
 					disableButtons(true);
@@ -178,8 +195,60 @@ public class SelectPracticeController extends Controller {
 		}
 	}
 
+	private void setLabelText(String curName) {
+		List<String> allNames = _originals.listNames();
+
+		String[] names = curName.split("[ -]");
+		List<Character> splits = extractSplits(curName);// Get all spaces and hyphens only
+		StringBuilder confirmed = new StringBuilder();
+		_names = new ArrayList<>();
+
+		if (names.length == 1) {
+			setNameText("Enter another name to combine", Color.BLACK, true);
+		} else {
+			for (int i = 0; i < names.length; i++) {
+				String name = names[i];
+				if (!containsName(name, allNames)) {
+					setNameText("\"" + name + "\" is not in the database", Color.RED, true);
+				} else {
+					name = name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase();
+					confirmed.append(name);
+					_names.add(name);
+					if (i >= splits.size()) {
+						confirmed.append(" ");
+					} else {
+						confirmed.append(splits.get(i));
+					}
+				}
+			}
+
+			String entered = curName.toUpperCase() + " ";
+			if (entered.equals(confirmed.toString().toUpperCase())) {
+				_newName = confirmed.toString();
+				setNameText(_newName, Color.GREEN, false);
+			}
+		}
+	}
+
+	private List<Character> extractSplits(String s) {
+		char[] temp = s.toCharArray();
+		List<Character> output = new ArrayList<>();
+		for (char c : temp) {
+			if (c == ' ' || c == '-') {
+				output.add(c);
+			}
+		}
+		return output;
+	}
+
+	private void setNameText(String text, Paint paint, boolean disableAdd) {
+		nameText.setText(text);
+		nameText.setTextFill(paint);
+		add.setDisable(disableAdd);
+	}
+
 	public void selectedSort(ActionEvent actionEvent) {
-		updatePreview(selectedOrder);
+		updatePreview(_selectedOrder);
 	}
 
 	public void alphabeticalSort(ActionEvent actionEvent) {
@@ -212,7 +281,7 @@ public class SelectPracticeController extends Controller {
 			if (uploadedNames.size() > 0) {
 				disableButtons(false);
 				previewList.getItems().addAll(uploadedNames);
-				selectedOrder.addAll(uploadedNames);
+				_selectedOrder.addAll(uploadedNames);
 			}
 		}
 	}
@@ -235,8 +304,8 @@ public class SelectPracticeController extends Controller {
 	}
 
 	private void updatePreview(List<String> list) {
-		currentPreviewList = list;
-		previewList.getItems().setAll(currentPreviewList);
+		_currentPreviewList = list;
+		previewList.getItems().setAll(_currentPreviewList);
 	}
 
 	protected static SelectPracticeController getInstance() {
@@ -250,10 +319,47 @@ public class SelectPracticeController extends Controller {
 	}
 
 	private void addName() {
-		String name = selectListView.getSelectionModel().getSelectedItem();
-		if (!containsName(name, previewList.getItems())) {
-			previewList.getItems().add(name);
-			selectedOrder.add(name);
+		String name = search.getText();
+		if (name != null && !name.contains(" ")) {
+			name = selectListView.getSelectionModel().getSelectedItem();
+			if (!containsName(name, previewList.getItems())) {
+				previewList.getItems().add(name);
+				_selectedOrder.add(name);
+			}
+		} else {
+			concatNames();
+		}
+	}
+
+	private void concatNames() {
+		String newFileName = _newName.replace(' ', '_');
+		if (Files.notExists(Paths.get("Temp/" + newFileName + ".wav"))) {
+
+			try {
+				Files.deleteIfExists(Paths.get("Temp/output.wav"));
+				Files.deleteIfExists(Paths.get("list.txt"));
+				Files.createFile(Paths.get("list.txt"));
+				for (String name : _names) {
+					String fileName = pickBestOriginal(name);
+
+					String dir = "file 'Names/" + name + "/Original/" + fileName + "'\n";
+					Files.write(Paths.get("list.txt"), dir.getBytes(), StandardOpenOption.APPEND);
+
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+			Media.concatNames(newFileName);
+			_originals.addConcat(new Original(_newName, newFileName + ".wav"));
+		}
+
+		if (SelectPracticeController.getInstance() == null) {
+			List<String> list = new ArrayList<>();
+			list.add(_newName);
+			_mediator.setPracticeMainList(list);
+		} else {
+			_INSTANCE.addValue(_newName);
 		}
 	}
 }
