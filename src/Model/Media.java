8f6faf9b2fc6e768f7a52b4bcb09eafe49a881d1
Eric Pedrido <epedrids@gmail.com>
2018-10-22 03:12:46 +1300

    Media fully commented.

--- src/Model/Media.java
+++ src/Model/Media.java
@@ -1,18 +1,19 @@
 package Model;
 
+import Control.SelectPracticeController;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.List;
 
 /**
  * Responsible for handling various interactions with the media
  * files referenced as {@link Original} and {@link Practice}.
  * Also contains methods allowing for a microphone test.
+ *
+ * @author Eric Pedrido
  */
 public class Media {
 
@@ -56,66 +57,44 @@ public class Media {
 	}
 
 	/**
-	 * Records for 3 seconds, giving the user time
-	 * to input a microphone test, saving the test to
-	 * <dir>temp.wav</dir>. When the 3 seconds
-	 * is finished, the audio is played back to the
-	 * user.
+	 * Concatenates names specified in <dir>list.txt</dir>
 	 *
-	 * @see #playbackMicTest()
+	 * @param outputName file name of the final output in format <q>(name 1)_(name 2)_ ... _(name i)_.wav</q>
+	 *                   for i names.
 	 */
-	public static void recordMicTest() {
-		String record = "ffmpeg -f alsa -i default -t 3 temp.wav";
-		process(record, CURRENT_DIRECTORY);
-		playbackMicTest();
-	}
-
-	/**
-	 * Plays the short microphone test back to the user,
-	 * confirming whether or not the user has a functioning
-	 * microphone. Once the user closes the mic test window,
-	 * <dir>temp.wav</dir> is deleted.
-	 *
-	 * @see #finishMicTest()
-	 */
-	private static void playbackMicTest() {
-			String record = "ffplay -autoexit -nodisp -i temp.wav";
-			process(record, CURRENT_DIRECTORY);
-			finishMicTest();
-	}
-
-	/**
-	 * Deletes the temporary audio file created by the user
-	 * when recording a test at {@link #recordMicTest()}.
-	 */
-	public static void finishMicTest() {
-		try {
-			Files.delete(new File("temp.wav").toPath());
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
 	public static void concatNames(String outputName) {
 		String command = "ffmpeg -f concat -safe 0 -i list.txt -c copy Temp/_output.wav";
 		process(command, CURRENT_DIRECTORY);
 		removeSilence(outputName);
 	}
 
+	/**
+	 * Normalizes the volume of the file. This is done by
+	 * first detecting the mean volume using the <q>volumedetect</q> filter from ffmpeg.
+	 * Then, reading the output and extracting just the {@code double} value from the piped output.
+	 * Then, calculating how far from the {@link #TARGET_VOLUME} the detected mean volume of the file was.
+	 * Then, adjusting the volume accordingly.
+	 *
+	 * @param file name of file to normalize.
+	 * @param num iteration number to save as. See {@link SelectPracticeController#concatNames()} for implementation.
+	 */
 	public static void normalizeVolume(String file, int num) {
 		try {
+			// Get Mean volume
 			String command = "ffmpeg -i " + file + " -filter:a volumedetect -f null /dev/null 2>&1 | grep mean_volume";
 			ProcessBuilder pb = new ProcessBuilder("/bin/bash", "-c", command);
 			Process process = pb.start();
 			process.waitFor();
 
+			// Read output from ffmpeg and extract just the volume.
 			BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()));
 			String volumeDetectOutput = br.readLine();
 			volumeDetectOutput = volumeDetectOutput.replaceAll(".*:", ""); // remove all unnecessary characters
 			double meanVolume = Double.valueOf(volumeDetectOutput.substring(1, volumeDetectOutput.lastIndexOf('d') - 1)); // Only get digits
-			double difference = TARGET_VOLUME - Math.abs(meanVolume);
-			System.out.println(difference);
 
+			double difference = TARGET_VOLUME - Math.abs(meanVolume);
+
+			// Adjust the volume based on how far it is from the target volume.
 			String applyAdjusmentCommand = "ffmpeg -i " +file + " -filter:a \"volume=" + Math.abs(difference) +
 					"dB\" Temp/_normalized" + num + ".wav";
 
@@ -125,6 +104,11 @@ public class Media {
 		}
 	}
 
+	/**
+	 * Uses the silenceremove filter from ffmpeg to trim any quiet patches in the already concatenated file.
+	 *
+	 * @param file final output filename. Naming nomenclature specified in {@link #concatNames(String)}.
+	 */
 	private static void removeSilence(String file) {
 		String command = "ffmpeg -y -i Temp/_output.wav -af silenceremove=0:0:0:-1:1:-50dB:1 " + "Temp/" + file + ".wav";
 		process(command, CURRENT_DIRECTORY);
